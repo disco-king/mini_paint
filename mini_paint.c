0e764a422be9a199a7f60d31417a5fca14b59b95
acalabash <acalabash@gmail.com>
2021-10-23 04:04:07 -0400

    working unoptimized

---
+++ mini_paint.c
@@ -0,0 +1,210 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int ft_printf(const char *format, ...);
+
+typedef struct fld{
+	int width;
+	int height;
+	char b_char;
+} t_field;
+
+typedef struct s_list{
+	char type;
+	double x;
+	double y;
+	double radius;
+	char c_char;
+	struct s_list *next;
+} l_list;
+
+void free_lst(l_list *lst)
+{
+	l_list *buff = lst;
+
+	lst = lst->next;
+	while(buff->next)
+	{
+		free(buff);
+		buff = lst;
+		lst = lst->next;
+	}
+	free(buff);
+}
+
+l_list *lst_new(l_list src)
+{
+	l_list *ret = (l_list *)malloc(sizeof(l_list));
+	if(!ret)
+		return(ret);
+	ret->type = src.type;
+	ret->x = src.x;
+	ret->y = src.y;
+	ret->radius = src.radius;
+	ret->c_char = src.c_char;
+	ret->next = NULL;
+	return(ret);
+}
+
+void lst_add(l_list *lst, l_list *new)
+{
+	l_list *ptr = lst;
+	while(ptr->next)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = new;
+}
+
+void free_arr(char **arr)
+{
+	char **ptr = arr;
+	while(*arr)
+	{
+		free(*arr);
+		arr++;
+	}
+	free(*arr);
+	free(ptr);
+}
+
+char **get_arr(t_field *field)
+{
+	char **arr = (char **)malloc((field->height + 1) * sizeof(char *));
+	arr[field->height] = NULL;
+	int i = 0;
+	while(i < field->height)
+	{
+		arr[i] = (char *)malloc((field->width + 1) * sizeof(char));
+		memset(arr[i], field->b_char, field->width);
+		arr[i][field->width] = 0;
+		i++;
+	}
+	return(arr);
+}
+
+int get_field(t_field *field, FILE *fd)
+{
+	int res;
+
+	res = fscanf(fd, "%d %d %c", &(field->width), &(field->height), &(field->b_char));
+	if(field->width < 0 || field->width > 300 ||
+		field->height < 0 || field->height > 300 ||
+		res != 3)
+		return(1);
+	return(0);
+}
+
+int get_circles(FILE *fd, l_list **lst)
+{
+	l_list l;
+
+	if(fscanf(fd, "\n%c %lf %lf %lf %c", &(l.type), &(l.x), &(l.y), &(l.radius), &(l.c_char)) != 5)
+		return(1);
+	l_list *start = lst_new(l);
+	if(!start)
+		return(1);
+	int res = fscanf(fd, "\n%c %lf %lf %lf %c", &(l.type), &(l.x), &(l.y), &(l.radius), &(l.c_char));
+	while(res == 5)
+	{
+		lst_add(start, lst_new(l));
+		res = fscanf(fd, "\n%c %lf %lf %lf %c", &(l.type), &(l.x), &(l.y), &(l.radius), &(l.c_char));
+	}
+	*lst = start;
+	if(res != -1)
+		return(1);
+	return(0);
+}
+
+int check_prox(l_list *lst, double y, double x)
+{
+	l_list *ptr = lst;
+	double dist;
+	int flag = 0;
+	double type;
+	while(ptr)
+	{
+		if(ptr->type == 'C')
+			type = 0;
+		else
+			type = 1;
+		dist = sqrt(((ptr->x - x) * (ptr->x - x)) + ((ptr->y - y) * (ptr->y - y)));
+		if(type == 1)
+		{
+			if(dist > ptr->radius - type && dist <= ptr->radius)
+				flag = ptr->c_char;
+		}
+		else
+			if(dist <= ptr->radius)
+				flag = ptr->c_char;
+		ptr = ptr->next;
+	}
+	return(flag);
+}
+
+void fill_arr(char **arr, l_list *lst)
+{
+	int height = 0;
+	int width;
+	int c;
+
+	while(arr[height])
+	{
+		width = 0;
+		while(arr[height][width])
+		{
+			c = check_prox(lst, (double)height, (double)width);
+			if(c)
+				arr[height][width] = c;
+			width++;
+		}
+		height++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		printf("arg error\n");
+		return(1);
+	}
+	FILE *fd = fopen(argv[1], "r");
+	if(!fd)
+	{
+		printf("bad file\n");
+		return(1);
+	}
+	t_field *field = (t_field *)malloc(sizeof(t_field));
+	if(get_field(field, fd))
+	{
+		free(field);
+		printf("bad file\n");
+		return(1);
+	}
+	l_list *lst;
+	if(get_circles(fd, &lst))
+	{
+		free_lst(lst);
+		free(field);
+		printf("bad file\n");
+		return(1);
+	}
+	char **field_arr = get_arr(field);
+	fill_arr(field_arr, lst);
+	char **buff = field_arr;
+	while(*buff)
+	{
+		printf("%s\n", *buff);
+		buff++;
+	}
+	free_arr(field_arr);
+	fclose(fd);
+	free_lst(lst);
+	free(field);
+	return(0);
+}
